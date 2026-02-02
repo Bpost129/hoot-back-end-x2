6669ad4677c0bdecc9517f3e91ceb9f100a11dbc
Bpost129 <Bpost129@yahoo.com>
2026-02-01 21:09:29 -0500

    add route and controller function to create a blog, test in postman

--- routes/blogs.js
+++ routes/blogs.js
@@ -4,9 +4,13 @@ import { decodeUserFromToken, checkAuth } from "../middleware/auth.js"
 
 const router = Router()
 
+// Unprotected Routes
+// router.get('/', blogsCtrl.index)
 
-
+// Protected Routes
 router.use(decodeUserFromToken)
+router.post('/', checkAuth, blogsCtrl.create)
+
 
 export { 
   router
