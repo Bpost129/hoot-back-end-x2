e8a282d0d8b794b169724df49d39cf71926bb120
Bpost129 <Bpost129@yahoo.com>
2026-02-01 20:46:23 -0500

    create comment schema and add to blog model

--- models/blog.js
+++ models/blog.js
@@ -2,6 +2,13 @@ import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
 
+const commentSchema = new Schema({
+  text: {type: String, required: true},
+  author: {type: Schema.Types.ObjectId, ref: 'Profile'}
+}, {
+  timestamps: true
+})
+
 const blogSchema = new Schema({
   title: {type: String, required: true},
   text: {type: String, required: true},
@@ -9,8 +16,9 @@ const blogSchema = new Schema({
     type: String, 
     required: true,
     enum: ['News', 'Sports', 'Games', 'Movies', 'Music', 'Television'],
-    author: { type: Schema.Types.ObjectId, ref: 'Profile' }
-  }
+  },
+  comments: [commentSchema],
+  author: { type: Schema.Types.ObjectId, ref: 'Profile' }
 }, {
   timestamps: true
 })
